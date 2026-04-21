7270a1a8c5f5bdce2229426ef996ac73c2bc515b
Lana Ryzhkova <73231664+lanaryzhkova@users.noreply.github.com>
2026-04-21 17:44:23 +0300

    Fix price string handling to remove commas in price conversion

--- utils/utils.py
+++ utils/utils.py
@@ -2,5 +2,5 @@ def prices_array_handle(prices_text):
         return [float(price.replace("$", "")) for price in prices_text]
 
 def price_str_handle(price_str):
-        return float(price_str.text.strip().replace("$", ""))
+        return float(price_str.text.strip().replace("$", "").replace(",", ""))
 
