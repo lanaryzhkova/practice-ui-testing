7f95e095a422da359cb6810c50af6de12558d2f6
Lana Ryzhkova <73231664+lanaryzhkova@users.noreply.github.com>
2026-04-21 14:25:34 +0300

    Add CartPage and Steps classes for cart functionality in UI testing

---
+++ pages/cart_page/steps.py
@@ -0,0 +1,29 @@
+import allure
+
+class Steps:
+    @allure.step("Загрузка страницы корзины")
+    def load_cart_page(self, cart_page):
+        cart_page.load()
+
+    @allure.step("Увеличение в 2 раза количества самого дешевого товара в корзине")
+    def increase_cheapest_product_quantity(self, cart_page):
+        cheapest_product = cart_page.cheapest_product()
+        current_quantity = cart_page.get_product_quantity(cheapest_product)
+        new_quantity = current_quantity * 2
+        cart_page.change_product_quantity(new_quantity, cheapest_product)
+        cart_page.click_update_cart()
+        return self
+    
+    @allure.step("Получение рассчитанной и фактической суммы корзины")
+    def get_cart_totals(self, cart_page):
+        calculated_total = cart_page.calculating_total_price()
+        cart_total = cart_page.get_cart_total()
+        return calculated_total, cart_total
+    
+    @allure.step("Удаление продукта из корзины")
+    def remove_product(self, cart_page):
+        cart_page.remove_even_products()
+
+    @allure.step("Получение списка всех продуктов в корзине")
+    def get_all_products(self, cart_page):
+        return cart_page.get_all_products_in_cart()
