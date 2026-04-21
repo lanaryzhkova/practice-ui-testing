b065b6bc0a6dddb1c55c6e2416a3bc09ca59550c
Lana Ryzhkova <73231664+lanaryzhkova@users.noreply.github.com>
2026-04-21 16:06:31 +0300

    Update Chrome options in driver fixture for headless testing

--- conftest.py
+++ conftest.py
@@ -5,9 +5,9 @@ import pytest
 @pytest.fixture
 def driver():
     options = Options()
-    options.add_argument("--headless")  # без UI
-    options.add_argument("--no-sandbox")
-    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--headless=new")
+    options.add_argument("--window-size=1920,1080")
+    options.add_argument("--disable-gpu")
 
     driver = webdriver.Chrome(options=options)
     yield driver
