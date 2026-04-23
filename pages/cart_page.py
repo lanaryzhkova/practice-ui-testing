dbe6117e7244c7dbc4d78c256346185310d16c0e
Lana Ryzhkova <73231664+lanaryzhkova@users.noreply.github.com>
2026-04-23 12:22:16 +0300

    fix

--- pages/cart_page.py
+++ pages/cart_page.py
@@ -42,7 +42,7 @@ class CartPage(BasePage):
     def open_cart_page(self) -> 'CartPage':
         """Открывает страницу корзины и ожидает загрузки URL"""
         self.open(CART_URL)
-        self.wait.wait_for_element_visible(self.products_list)
+        self.wait.wait_for_url(CART_URL)
         py_logger.info(f"Загружена страница {CART_URL}")
         return self
 
