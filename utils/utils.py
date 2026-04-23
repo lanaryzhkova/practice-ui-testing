6e1552613164c5b1cceee5d0eaf1832a7c1682d0
Lana Ryzhkova <73231664+lanaryzhkova@users.noreply.github.com>
2026-04-23 03:47:06 +0300

    added typing

--- utils/utils.py
+++ utils/utils.py
@@ -1,6 +1,8 @@
-def prices_array_handle(prices_text):
+from selenium.webdriver.remote.webelement import WebElement
+
+def prices_array_handle(prices_text: list[str]) -> list[float]:
         return [float(price.replace("$", "")) for price in prices_text]
 
-def price_str_handle(price_str):
+def price_str_handle(price_str: WebElement) -> float:
         return float(price_str.text.strip().replace("$", "").replace(",", ""))
 
